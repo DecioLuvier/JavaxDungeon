006cf00f3323c01850eb3704402022c7c9d4555b
DecioLuvier <142108217+DecioLuvier@users.noreply.github.com>
2025-06-29 11:35:16 -0300

    Update Background.java

--- src/game/gui/background/Background.java
+++ src/game/gui/background/Background.java
@@ -6,7 +6,7 @@ import engine.Level;
 import engine.actors.VisualActor;
 
 public class Background extends VisualActor {
-    private static SpriteSheet spriteSheet = new SpriteSheet("src/game/actors/gui/background/Background.png", 1, 0, 0, 0);
+    private static SpriteSheet spriteSheet = new SpriteSheet("src/game/gui/background/Background.png", 1, 0, 0, 0);
 
     public Background(Level level, int x, int y) {
         super(level, new Animation(spriteSheet, 0, 1, 1), x, y);
